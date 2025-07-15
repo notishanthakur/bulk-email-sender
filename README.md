d1d996a78e345c840ac0d2c70c5663d65dcf4fcb
notishanthakur <ishanst.shares@gmail.com>
2025-07-16 02:49:07 +0530

    Finalizing script version

--- README.md
+++ README.md
@@ -2,20 +2,31 @@
 This is a simple Python script to send bulk emails using your email id and a csv sheet as input
 
 # How to use?
+## Getting credentials
+### For Google
+  1. Enable 2FA
+  2. Go to https://myaccount.google.com/apppasswords and generate an app password
+  3. Use this password in .env
+  4. Accordingly select smtp and port in .env
+  
 ## On WSL/LINUX based terminal
   1. Clone the repository using `git clone https://github.com/notishanthakur/bulk-email-sender`
   2. Navigate to the folder using `cd bulk-email-sender`
-  3. In config.env, add your credentials
+  3. In .env, add your credentials
   4. According to the input.csv, fill in the data (refer to customization)
-  5. Run the script using python3 `python3 script.py`
+  5. Install python-dotenv using `pip install python-dotenv`
+  6. Run the script using python3 `python3 script.py`
 
 # Customization
   Fork the repo and modify script.py according to your requirements (easy to read and modify) <br>
   Similarly, also modify input.csv
 
 # Development Info
-Made using python3 and imports libraries: csv to parse info, smtp to make a connection and send mail
+Made using python3 and imports libraries: 
+  1. csv to parse info
+  2. smtp to make a connection and send mail
+  3. email to craft the message to be sent
 
 # Future Roadmap?
-GUI Interface
+GUI Interface <br>
 (Avoiding online deployment due to security factors)
--- script.py
+++ script.py
@@ -1,25 +1,66 @@
-import csv,smtplib
+import csv, smtplib
 
+from dotenv import load_dotenv
+import os
 
-input_path = 'input.csv' #enter path here
-emailhandler = smtplib.SMTP('',) #'smtp', port
-email_id = ""  #enter email
-password = ""  #enter password
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+load_dotenv()
+
+input_path = os.getenv("input_path")
+emailHandler = smtplib.SMTP(os.getenv("smtp"), os.getenv("port"))
+email_id = os.getenv("email_id")
+password = os.getenv("password")
 
 try:
-    emailhandler.starttls()
+    emailHandler.starttls()
 
 except Exception as e:
     print("Error initialising email service ", e)
 
-emailhandler.login(email_id, password)
+emailHandler.login(email_id, password)
 
 try:
     with open(input_path, 'r', newline='') as file:
         reader = csv.reader(file)
-        header = next(reader) 
+        header = next(reader)
+
+        count = 0
+
         for row in reader:
-            print(row)
+            
+            
+#constructing message
+            msg = MIMEMultipart()            
+            msg['From'] = email_id
+            msg['To'] = row[0]
+            
+            msg['Subject'] = "Congrulations for participating at " + row[2]
+            
+            body = "Hi " + row[1] + "\n" + "Congratulations for " + row[3] + " in " + row[2] + " at " + row[4] + "\nThe certificate is attached below\n\nThank you"
+            
+            msg.attach(MIMEText(body, 'plain'))
+
+            certificate = row[5]
+            attachment = open("./certificates/" + row[5], "rb")
+            new = MIMEBase('application', 'octet-stream')
+            new.set_payload((attachment).read())
+            encoders.encode_base64(new)
+            new.add_header('Content-Disposition', "attachment; filename= %s" %certificate)
+            msg.attach(new)
+
+            text = msg.as_string()
+            emailHandler.sendmail(email_id, row[0], text)
+            print("Sent email to: " + row[0])
+            count+=1
+
+
+        print("/nTotal " + count + " emails sent")
+        
+        emailHandler.quit()
 
 except FileNotFoundError:
     print("File not found on path ", input_path)
